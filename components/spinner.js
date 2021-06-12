a570920dc73b481d28c53d5f509f3aa4b794fb00
BasileNq <basile.n@orange.fr>
2021-06-12 11:37:15 +0200

    feat: add text hover and increase icon's hitbox

--- components/spinner.js
+++ components/spinner.js
@@ -1,20 +1,21 @@
 import * as React from 'react';
 import { useState } from "react";
-import {Flex, Box, Image} from "@chakra-ui/react"
+import {Flex, Image, Text} from "@chakra-ui/react"
 import Link from "next/link"
 
 const Spinner = (props) => {
     const [space, setSpace] = useState(false);
-    const [rotation, setRotation] = useState(15);
-    const [rotation2, setRotation2] = useState(15);
+    const [rotation, setRotation] = useState(30);
+    const [rotation2, setRotation2] = useState(30);
+    const [text, setText] = useState("");
     // const rotate = () => {
     //     setRotation(rotation+5)
     // }
 
     const Space = () =>{
         if (space) {
-            setRotation(15);
-            setRotation2(15);
+            setRotation(30);
+            setRotation2(30);
             setSpace(false);
         }else{
             setRotation(3);
@@ -22,19 +23,28 @@ const Spinner = (props) => {
             setSpace(true);
         }
     }
+
+    const handleMaouseEnter = (textInput) => {
+        setText(textInput);
+    }
+    const handleMaouseLeave = (textInput) => {
+        setText("");
+    }
     return(
         // Glocal Flex -> Rox direction 
+        <>
+        <Text fontSize="5xl" color="black" fontFamily="cursive"  position="absolute" >{text}</Text>
         <Flex alignSelf="center" style={{animation: `spin ${rotation}s linear infinite`}} flexDirection="row" cursor="pointer">
             {/* Left icons */}
             <Flex flexDirection="column" justifyContent="space-evenly">
-                <Flex >
+                <Flex boxSize={{lg:"10vh"}} onMouseEnter={(textInput)=>handleMaouseEnter("")} onMouseLeave={() => handleMaouseLeave()}>
                     <Link href="/">
-                        <Image  style={{animation: `reversespin ${rotation2}s linear infinite`}} src="/icons/contour-de-dessin-anime-de-hibou.svg" height={{ base: 7, md: 25, lg:50 }} width={{ base: 7, md: 25, lg:50 }} />
+                        <Image position="relative" top={{lg: "4vh"}} left={{lg: "4vh"}} style={{animation: `reversespin ${rotation2}s linear infinite`}} src="/icons/contour-de-dessin-anime-de-hibou.svg" height={{ base: 7, md: 25, lg:50 }} width={{ base: 7, md: 25, lg:50 }} />
                     </Link>
                 </Flex>
-                <Flex>
+                <Flex boxSize={{lg:"10vh"}} onMouseEnter={(textInput)=>handleMaouseEnter("Linkedin")} onMouseLeave={() => handleMaouseLeave()} >
                     <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/basile-nonclercq/">
-                        <Image style={{animation: `reversespin ${rotation2}s linear infinite`}}   src="/icons/logo-linkedin.svg" height={{ base: 7, md: 25, lg:50 }} width={{ base: 7, md: 25, lg:50 }} />
+                        <Image position="relative" left={{lg: "4vh"}} style={{animation: `reversespin ${rotation2}s linear infinite`}}   src="/icons/logo-linkedin.svg" height={{ base: 7, md: 25, lg:50 }} width={{ base: 7, md: 25, lg:50 }} />
                     </a>
                 </Flex>
             </Flex>
@@ -42,14 +52,14 @@ const Spinner = (props) => {
             <Flex alignSelf="center"  flexDirection="column" >
                 {/* top icons */}
                 <Flex alignSelf="center"  w="100%" justifyContent="space-around">
-                    <Flex >
+                    <Flex boxSize={{lg:"10vh"}} onMouseEnter={(textInput)=>handleMaouseEnter("Github")} onMouseLeave={() => handleMaouseLeave()} >
                         <a target="_blank" rel="noopener noreferrer" href="https://github.com/BasileNq">
-                            <Image style={{animation: `reversespin ${rotation2}s linear infinite`}}  src="/icons/signe-github.svg" height={{ base: 7, md: 25, lg:50 }} width={{ base: 7, md: 25, lg:50 }} />
+                            <Image position="relative" top={{lg: "4vh"}} left={{lg: "4vh"}} style={{animation: `reversespin ${rotation2}s linear infinite`}}  src="/icons/signe-github.svg" height={{ base: 7, md: 25, lg:50 }} width={{ base: 7, md: 25, lg:50 }} />
                         </a>
                     </Flex>
-                    <Flex >
+                    <Flex boxSize={{lg:"10vh"}} onMouseEnter={(textInput)=>handleMaouseEnter("4L Trophy")} onMouseLeave={() => handleMaouseLeave()}  >
                         <Link href="/4ltrophy">
-                            <Image style={{animation: `reversespin ${rotation2}s linear infinite`}}   src="/icons/contour-de-vue-cote-hibou.svg" height={{ base: 7, md: 25, lg:50 }} width={{ base: 7, md: 25, lg:50 }} />
+                            <Image position="relative" top={{lg: "4vh"}} style={{animation: `reversespin ${rotation2}s linear infinite`}}   src="/icons/contour-de-vue-cote-hibou.svg" height={{ base: 7, md: 25, lg:50 }} width={{ base: 7, md: 25, lg:50 }} />
                         </Link>
                     </Flex>
                 </Flex>
@@ -59,30 +69,31 @@ const Spinner = (props) => {
                 </Flex>
                 {/* bottom icons */}
                 <Flex alignSelf="center"  w="100%" justifyContent="space-around">
-                    <Flex  >
+                    <Flex boxSize={{lg:"10vh"}} onMouseEnter={(textInput)=>handleMaouseEnter("Work Blog")} onMouseLeave={() => handleMaouseLeave()}  >
                         <Link href="/work-blog/">    
-                            <Image style={{animation: `reversespin ${rotation2}s linear infinite`}}  src="/icons/hibou.svg" height={{ base: 7, md: 25, lg:50 }} width={{ base: 7, md: 25, lg:50 }} />
+                            <Image position="relative" left={{lg: "4vh"}} style={{animation: `reversespin ${rotation2}s linear infinite`}}  src="/icons/hibou.svg" height={{ base: 7, md: 25, lg:50 }} width={{ base: 7, md: 25, lg:50 }} />
                         </Link>                            
                     </Flex>
-                    <Flex >
-                        <Image style={{animation: `reversespin ${rotation2}s linear infinite`}}  src="/icons/blog.svg" height={{ base: 7, md: 25, lg:50 }} width={{ base: 7, md: 25, lg:50 }} />
+                    <Flex boxSize={{lg:"10vh"}} onMouseEnter={(textInput)=>handleMaouseEnter("")} onMouseLeave={() => handleMaouseLeave()}  >
+                        <Image position="relative" style={{animation: `reversespin ${rotation2}s linear infinite`}}  src="/icons/blog.svg" height={{ base: 7, md: 25, lg:50 }} width={{ base: 7, md: 25, lg:50 }} />
                     </Flex>
                 </Flex>
             </Flex>
             {/* right icons */}
             <Flex flexDirection="column" justifyContent="space-evenly">
-                <Flex >
+                <Flex boxSize={{lg:"10vh"}} onMouseEnter={(textInput)=>handleMaouseEnter("Resume")} onMouseLeave={() => handleMaouseLeave()}  >
                     <Link href="cv.pdf">
-                        <Image style={{animation: `reversespin ${rotation2}s linear infinite`}}  src="/icons/cv-et-cv.svg" height={{ base: 7, md: 25, lg:50 }} width={{ base: 7, md: 25, lg:50 }} />
+                        <Image position="relative" top={{lg: "4vh"}} style={{animation: `reversespin ${rotation2}s linear infinite`}}  src="/icons/cv-et-cv.svg" height={{ base: 7, md: 25, lg:50 }} width={{ base: 7, md: 25, lg:50 }} />
                     </Link>
                 </Flex>
-                <Flex>
+                <Flex boxSize={{lg:"10vh"}} onMouseEnter={(textInput)=>handleMaouseEnter("Mess Blog")} onMouseLeave={() => handleMaouseLeave()} >
                     <Link href="/hobby-blog/">
                         <Image style={{animation: `reversespin ${rotation2}s linear infinite`}}   src="/icons/hibouFooter.png" height={{ base: 7, md: 25, lg:50 }} width={{ base: 7, md: 25, lg:50 }} />
                     </Link>
                 </Flex>
             </Flex>
-        </Flex>        
+        </Flex>      
+    </>  
     );
 }
 
