9d4a0d8dd2b23976a3ade9ca8fab937778b0f7a7
BasileNq <basile.n@orange.fr>
2021-06-12 10:38:34 +0200

    feat: spinner while images are loading

--- pages/4ltrophy.js
+++ pages/4ltrophy.js
@@ -1,8 +1,9 @@
-import {Image, Box, Flex, Spacer, Center, Text} from "@chakra-ui/react";
+import {Image, Box, Flex, Spacer, Center, Text, Spinner} from "@chakra-ui/react";
 import Header from "../components/header"
 import Footer from "../components/footer"
 import {getNbPictures} from "../lib/trophy"
 import ToHome from "../components/toHome"
+import { useState } from "react";
 
 
 export async function getStaticProps(){
@@ -14,7 +15,13 @@ export async function getStaticProps(){
     }
 }
 
+
+
 const Trophy = ({images}) => {
+    const [loaded, setLoaded] = useState(false)
+    const imageLoaded = () => {
+        setLoaded(true);
+    }
     return (
         <>
         <Header title="Raid: 4L Trophy" icon1="/icons/messOwl.webp" icon2="/icons/messOwl-return.webp"></Header>
@@ -23,8 +30,16 @@ const Trophy = ({images}) => {
             <Flex width="80%" flexDirection="column">
             {images.map((src, index) => (
             <Box  marginTop="2em"  alignItems="center">
-                <Flex flexDirection="row" justify="space-evenly" alignSelf="center">
-                    <Image borderRadius="xl" width="30vw" height="30vh" objectFit="cover" src={src[0]} alt="image 4L Trophy"></Image>
+                <Flex flexDirection="row" justify="space-evenly" alignSelf="center" display={loaded?"none":"flex"}>
+                    <Box textAlign="center" width="30vw" height="30vh">
+                        <Spinner thickness="2px" speed="0.75s" color="black"size="md" label="Images current loaded"></Spinner>    
+                    </Box>
+                    <Box textAlign="center" width="30vw" height="30vh">
+                        <Spinner thickness="2px" speed="0.75s" color="black"size="md" label="Images current loaded"></Spinner>    
+                    </Box>
+                </Flex>
+                <Flex flexDirection="row" justify="space-evenly" alignSelf="center" display={loaded?"flex":"none"}>
+                    <Image borderRadius="xl" width="30vw" height="30vh" objectFit="cover" src={src[0]} onLoad={imageLoaded} alt="image 4L Trophy"></Image>
                     <Image borderRadius="xl" width="30vw" height="30vh" objectFit="cover" src={src[1]} alt="image 4L Trophy"></Image>
                 </Flex>
             </Box>
