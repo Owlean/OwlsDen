322953ffbf139e2c69c9bed6475f2389dbcee5fb
BasileNq <basile.n@orange.fr>
2022-05-21 20:10:49 +0200

    feat: tmp download source

---
+++ pages/tmp.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+import {Text} from "@chakra-ui/react"
+
+export default function tmpExam() {
+  return (
+    <>
+	 <Link href="scrabbleNonclercqBasile.zip">
+<Text>Download Scrabble</Text>
+        </Link>
+    </>
+
+  )
+}
